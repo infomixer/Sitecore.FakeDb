1a9794081847d45df20d55be567f7ab1e129bb1b
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-01-02 03:43:54 +0200

    Add DefaultProviderSwitcherFactory

---
+++ Source/Sitecore.FakeDb.Tests/DefaultProviderSwitcherFactoryTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.FakeDb.Tests
+{
+  using FluentAssertions;
+  using Xunit;
+
+  public class DefaultProviderSwitcherFactoryTest
+  {
+    [Fact]
+    public void ShouldBeProviderSwitcherFactory()
+    {
+      // arrange
+      var factory = new DefaultProviderSwitcherFactory();
+
+      // assert
+      factory.Should().BeAssignableTo<IProviderSwitcherFactory>();
+    }
+  }
+}
---
+++ Source/Sitecore.FakeDb/DefaultProviderSwitcherFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Sitecore.FakeDb
+{
+  using System;
+
+  public class DefaultProviderSwitcherFactory : IProviderSwitcherFactory
+  {
+    public IDisposable Create(Type switcherType, object provider)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
