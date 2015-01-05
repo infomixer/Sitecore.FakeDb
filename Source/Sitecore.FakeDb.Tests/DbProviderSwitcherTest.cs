47cdb4a6cf40bbeae0c9928f0c8977faacac9dba
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-01-05 02:52:32 +0200

    Dispose switched providers on db.Dispose()

--- Source/Sitecore.FakeDb.Tests/DbProviderSwitcherTest.cs
+++ Source/Sitecore.FakeDb.Tests/DbProviderSwitcherTest.cs
@@ -2,28 +2,54 @@
 {
   using FluentAssertions;
   using NSubstitute;
+  using Sitecore.Data;
   using Sitecore.Links;
+  using Sitecore.Security.Accounts;
+  using System.Web.Security;
   using Xunit;
 
   public class DbProviderSwitcherTest
   {
     [Fact]
-    public void ShouldSwitchLinkDatabase()
+    public void ShouldSwitchAndDisposeLinkDatabase()
     {
       // arrange
       var provider = Substitute.For<LinkDatabase>();
+      var database = Database.GetDatabase("master");
+      var brokenLinks = new ItemLink[] { };
+
+      provider.GetBrokenLinks(database).Returns(brokenLinks);
 
       using (var db = new Db())
       {
-        var brokenLinks = new ItemLink[] { };
-        provider.GetBrokenLinks(db.Database).Returns(brokenLinks);
-
         // act
         db.Providers.Switch<LinkDatabase>(provider);
 
         // assert
-        Globals.LinkDatabase.GetBrokenLinks(db.Database).Should().BeSameAs(brokenLinks);
+        Globals.LinkDatabase.GetBrokenLinks(database).Should().BeSameAs(brokenLinks);
       }
+
+      Globals.LinkDatabase.GetBrokenLinks(database).Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ShouldSwitchAndDisposeRoleProvider()
+    {
+      // arrange
+      var provider = Substitute.For<RoleProvider>();
+      var roles = new[] { "sitecore\\Developers" };
+      provider.GetAllRoles().Returns(roles);
+
+      using (var db = new Db())
+      {
+        // act
+        db.Providers.Switch<RoleProvider>(provider);
+
+        // assert
+        Roles.GetAllRoles().Should().BeSameAs(roles);
+      }
+
+      Roles.GetAllRoles().Should().BeEmpty();
     }
   }
 }
---
+++ Source/Sitecore.FakeDb/Pipelines/ReleaseFakeDb/DisposeDbProviderSwitchers.cs
@@ -0,0 +1,19 @@
+﻿namespace Sitecore.FakeDb.Pipelines.ReleaseFakeDb
+{
+  using Sitecore.Diagnostics;
+
+  public class DisposeDbProviderSwitchers
+  {
+    public virtual void Process(ReleaseDbArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+
+      if (args.Db.Providers == null)
+      {
+        return;
+      }
+
+      args.Db.Providers.Dispose();
+    }
+  }
+}
