9500d8b94985507949bf4a8c628545d0914d3211
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-03-07 03:12:56 +0200

    Add simple autofixture customization sample

---
+++ src/Sitecore.FakeDb.Tests/AutoFixtureTest.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.FakeDb.Tests
+{
+  using FluentAssertions;
+  using Ploeh.AutoFixture;
+  using Xunit;
+
+  public class AutoFixtureTest
+  {
+    [Fact]
+    public void ShouldDeserializeItems()
+    {
+      // arrange
+      var fixture = new Fixture();
+      fixture.Customize<Db>(d => d.Do(x => x.Add(new DbItem("home"))));
+
+      // act
+      using (var db = fixture.Create<Db>())
+      {
+        // assert
+        db.GetItem("/sitecore/content/home").Should().NotBeNull();
+      }
+    }
+  }
+}
