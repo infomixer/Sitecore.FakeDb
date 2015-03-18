ccedb1b8804d418ca7babf876ecc06177ade6f8b
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-03-18 22:56:40 +0200

    Do not reset factory on dispose

--- src/Sitecore.FakeDb.Tests/Data/FakeStandardValuesProviderTest.cs
+++ src/Sitecore.FakeDb.Tests/Data/FakeStandardValuesProviderTest.cs
@@ -1,9 +1,6 @@
 ﻿namespace Sitecore.FakeDb.Tests.Data
 {
-  using System;
   using FluentAssertions;
-  using NSubstitute;
-  using Sitecore.Configuration;
   using Sitecore.Data;
   using Sitecore.Data.Fields;
   using Sitecore.FakeDb.Data;
@@ -11,30 +8,27 @@
   using Sitecore.FakeDb.Data.Items;
   using Xunit;
 
-  public class FakeStandardValuesProviderTest : IDisposable
+  public class FakeStandardValuesProviderTest
   {
     private readonly FakeStandardValuesProvider provider;
-
-    private readonly Database database;
 
     public FakeStandardValuesProviderTest()
     {
       this.provider = new FakeStandardValuesProvider();
-      this.database = Factory.GetDatabase("master");
     }
 
     [Fact]
     public void ShouldRequireDataStorage()
     {
       // act & assert
-      provider.Should().BeAssignableTo<IRequireDataStorage>();
+      this.provider.Should().BeAssignableTo<IRequireDataStorage>();
     }
 
     [Fact]
     public void ShouldSetDataStorage()
     {
       // arrange
-      var storage = Substitute.For<DataStorage>(this.database);
+      var storage = new DataStorage();
 
       // act
       ((IRequireDataStorage)this.provider).SetDataStorage(storage);
@@ -47,18 +41,13 @@
     public void ShouldReturnEmptyStringIfNoTemplateFound()
     {
       // arrange
-      var storage = Substitute.For<DataStorage>(this.database);
+      var storage = new DataStorage();
       ((IRequireDataStorage)this.provider).SetDataStorage(storage);
 
-      var field = new Field(ID.NewID, ItemHelper.CreateInstance(this.database));
+      var field = new Field(ID.NewID, ItemHelper.CreateInstance());
 
       // act & assert
       this.provider.GetStandardValue(field).Should().BeEmpty();
-    }
-
-    public void Dispose()
-    {
-      Factory.Reset();
     }
   }
 }
